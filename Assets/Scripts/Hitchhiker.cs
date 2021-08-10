7cdc12b4c9d4d90850107d7ef301148437ef323d
okaybenji <okaybenji@gmail.com>
2021-08-10 12:27:11 -0500

    music starts when u pick up a passenger

--- Assets/Scripts/Hitchhiker.cs
+++ Assets/Scripts/Hitchhiker.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 public class Hitchhiker : MonoBehaviour {
   GameObject _passenger;
   GameObject _talkBubble;
+  GameObject _radio;
   void OnEnable() {
     _passenger = Utils.Find("Passenger");
+    _radio = Utils.Find("Radio");
   }
 
   void OnTriggerEnter() {
+    // Show the passenger.
     _passenger.SetActive(true);
+
+    // Play the sound of them getting in the car.
     _passenger.GetComponent<AudioSource>().Play();
+
+    // Start up the radio music.
+    _radio.GetComponent<RadioController>().Play();
+
+    // Hide the hitchhiker.
     gameObject.SetActive(false);
   }
 }
